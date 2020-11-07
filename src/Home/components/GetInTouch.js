138cfccd2470409c7b4eb35de4fa79205ef017fe
ddnazzah <dieudonnenazzah@yahoo.co.uk>
2020-11-07 00:39:37 +0000

    changed footer color

--- src/Home/components/GetInTouch.js
+++ src/Home/components/GetInTouch.js
@@ -2,36 +2,43 @@
  * @Author: Dieu-Donne Nazzah
  * @Date: 2020-06-08 20:23:21
  * @Last Modified by: Dieu-Donne Nazzah
- * @Last Modified time: 2020-07-18 01:06:27
+ * @Last Modified time: 2020-11-07 00:37:32
  */
 
-import React from 'react';
-import bg_1 from '../../_shared/assets/images/bg_1.jpg';
+import React from "react";
+import bg_1 from "../../_shared/assets/images/bg_1.jpg";
 
 const GetInTouch = () => {
-	return (
-		<section className='ddnazzah-section ddnazzah-get-in-touch img margin-top' style={{ backgroundImage: `url(${bg_1})` }}>
-			<div className='overlay'></div>
-			<div className='container'>
-				<div className='row justify-content-center'>
-					<div className='col-md-7  text-center' data-aos='fade-up'>
-						<h2>
-							I'm <span>Available</span> for employment / research
-						</h2>
-						<p className='text-white'>
-							Solution-driven professional excelling in a highly collaborative work environment, finding solutions
-							to challenges and focused on customer satisfaction
-						</p>
-						<p className='mb-0'>
-							<a href='#contact-section' className='btn btn-primary btn-outline-primary py-3 px-5'>
-								Get in touch
-							</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section
+            className="ddnazzah-section ddnazzah-get-in-touch img margin-top"
+            style={{ backgroundImage: `url(${bg_1})` }}
+        >
+            <div className="overlay"></div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-7  text-center" data-aos="fade-up">
+                        <h2>
+                            I'm <span>Available</span> for employment / research
+                        </h2>
+                        <p className="text-white">
+                            Solution-driven professional excelling in a highly
+                            collaborative work environment, finding solutions to
+                            challenges and focused on customer satisfaction
+                        </p>
+                        <p className="mb-0">
+                            <a
+                                href="#contact-section"
+                                className="btn btn-primary btn-outline-primary py-3 px-5"
+                            >
+                                Get in touch
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default GetInTouch;
