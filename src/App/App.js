cc30500a9d1207caff95a3169f0030ffc8cfe2eb
ddnazzah <dieudonnenazzah@yahoo.co.uk>
2020-11-17 03:06:15 +0000

    changed to react-tree-timeline

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-import { ToastProvider } from 'react-toast-notifications';
-import store from '../configureStore';
-import { Home } from '../Home';
-import { rrfProps } from '../_shared/constants';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React from "react";
+import { Provider } from "react-redux";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { ToastProvider } from "react-toast-notifications";
+import store from "../configureStore";
+import { Home } from "../Home";
+import { rrfProps } from "../_shared/constants";
 
 const App = () => {
-	return (
-		<Provider store={store}>
-			<ReactReduxFirebaseProvider {...rrfProps}>
-				<ToastProvider>
-					<Home />
-				</ToastProvider>
-			</ReactReduxFirebaseProvider>
-		</Provider>
-	);
+    AOS.init();
+
+    return (
+        <Provider store={store}>
+            <ReactReduxFirebaseProvider {...rrfProps}>
+                <ToastProvider>
+                    <Home />
+                </ToastProvider>
+            </ReactReduxFirebaseProvider>
+        </Provider>
+    );
 };
 
 export default App;
--- src/Home/components/Resume.js
+++ src/Home/components/Resume.js
@@ -2,28 +2,44 @@
  * @Author: Dieu-Donne Nazzah
  * @Date: 2020-06-08 18:24:26
  * @Last Modified by: Dieu-Donne Nazzah
- * @Last Modified time: 2020-10-16 02:07:03
+ * @Last Modified time: 2020-11-17 03:01:22
  */
 
-import React from 'react';
+import React from "react";
+import Timeline from "react-tree-timeline";
 
 const Resume = ({ data }) => {
-	return (
-		<section className='ddnazzah-section ddnazzah-no-pb' id='resume-section'>
-			<div className='container'>
-				<div className='row justify-content-center'>
-					<div className='col-md-10 heading-section text-center ' data-aos='fade-up'>
-						<h1 className='big big-2'>Resume</h1>
-						<h2 className='mb-4'>Resume</h2>
-						<p>
-							Solution-driven professional excelling in a highly collaborative work environment, finding solutions
-							to challenges and focused on customer satisfaction
-						</p>
-					</div>
-				</div>
-				<div className='row'>
-					<div className='col-12'>
-						<div className='resume-wrap '>
+    return (
+        <section
+            className="ddnazzah-section ddnazzah-no-pb"
+            id="resume-section"
+        >
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div
+                        className="col-md-10 heading-section text-center "
+                        data-aos="fade-up"
+                    >
+                        <h1 className="big big-2">Resume</h1>
+                        <h2 className="mb-4">Resume</h2>
+                        <p>
+                            Solution-driven professional excelling in a highly
+                            collaborative work environment, finding solutions to
+                            challenges and focused on customer satisfaction
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <Timeline
+                            data={data}
+                            wrapperClassName="tl-wrapper"
+                            descriptionClassName="tl-description"
+                            titleClassName="tl-title"
+                            timelineClassName="tl-timeline"
+                            dateClassName="tl-date"
+                        />
+                        {/* <div className='resume-wrap '>
 							<ol className='ol-timeline'>
 								{data.map((resume) => (
 									<li className='tl-item' data-aos='fade-up' key={resume.id}>
@@ -37,26 +53,26 @@ const Resume = ({ data }) => {
 									</li>
 								))}
 							</ol>
-						</div>
-					</div>
-				</div>
-				<div className='row justify-content-center mt-5'>
-					<div className='col-md-6 text-center' data-aos='fade-up'>
-						<p>
-							<a
-								href='https://drive.google.com/file/d/1wyWt80my9HUC3hsxTSSLB5FLq64Vp5Kj/view?usp=sharing'
-								className='btn btn-primary btn-outline-primary py-4 px-5'
-								target='_blank'
-								rel='noopener noreferrer'
-							>
-								Resume
-							</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+						</div> */}
+                    </div>
+                </div>
+                <div className="row justify-content-center mt-5">
+                    <div className="col-md-6 text-center" data-aos="fade-up">
+                        <p>
+                            <a
+                                href="https://drive.google.com/file/d/1wyWt80my9HUC3hsxTSSLB5FLq64Vp5Kj/view?usp=sharing"
+                                className="btn btn-primary btn-outline-primary py-4 px-5"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Resume
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Resume;
