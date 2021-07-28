1b7085db6ba78d0d3c510bed52cf10280f2a8470
Nazzah Dieu-Donne <dieudonnenazzah@yahoo.co.uk>
2021-07-28 04:36:34 +0000

    Updates

---
+++ precache-manifest.c88fe6d28cc06816822183f41007c94f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0dbcc2cf2db5bd4a9b2d9f21ee537a6",
+    "url": "/ddnazzah.github.io/index.html"
+  },
+  {
+    "revision": "1629f294a6c6d0aa2f72",
+    "url": "/ddnazzah.github.io/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "1629f294a6c6d0aa2f72",
+    "url": "/ddnazzah.github.io/static/js/2.75836864.chunk.js"
+  },
+  {
+    "revision": "23aeb84399ec92e640277e0517a6f03f",
+    "url": "/ddnazzah.github.io/static/js/2.75836864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df27f207f9066102a40",
+    "url": "/ddnazzah.github.io/static/js/main.6c691f0b.chunk.js"
+  },
+  {
+    "revision": "5d9275f313e569ccd014",
+    "url": "/ddnazzah.github.io/static/js/runtime-main.9a3c75aa.js"
+  },
+  {
+    "revision": "020f8848d16c14664841a13563fb385a",
+    "url": "/ddnazzah.github.io/static/media/bg_1.020f8848.jpg"
+  },
+  {
+    "revision": "97331232981b4a8c2dc624476bacdcab",
+    "url": "/ddnazzah.github.io/static/media/ddnazzah.logo.97331232.png"
+  },
+  {
+    "revision": "962d004c0e16071302a6cc340ab386f5",
+    "url": "/ddnazzah.github.io/static/media/me_1.962d004c.png"
+  }
+]);
